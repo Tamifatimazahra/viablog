12c68ae82b5018b98ae3e0c39c0a1f215d082399
Tamifatimazahra <tamifatimazahra57@gmail.com>
2026-02-03 16:23:00 +0100

    remplir la formule

--- home.js
+++ home.js
@@ -1,4 +1,12 @@
-let form = document.getElementById("formulaire");
-form.onclick =function(){
-    
-}
+let title =document.getElementById('title');
+let titre =document.getElementById('titre');
+let destination =document.getElementById('destination');
+let note =document.getElementById('note');
+let categorie =document.getElementById('catego');
+let submit =document.getElementById('submit');
+let Annuler =document.getElementById('Annuler');
+
+
+
+
+
