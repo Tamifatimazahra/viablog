72ee0779cbe4a72458eebdf57427344ff80aa689
Tamifatimazahra <tamifatimazahra57@gmail.com>
2026-02-04 10:07:47 +0100

    ajouter button add dans html et js

--- home.js
+++ home.js
@@ -1,12 +1,29 @@
-let title =document.getElementById('title');
+  let tab=[];
+  function enregister () {let title =document.getElementById('title');
 let titre =document.getElementById('titre');
 let destination =document.getElementById('destination');
 let note =document.getElementById('note');
-let categorie =document.getElementById('catego');
+let categorie =document.getElementById('categorie');
 let submit =document.getElementById('submit');
 let Annuler =document.getElementById('Annuler');
+let newdest={
+            title:title.value,
+            titre:titre.value,
+            destination:destination.value,
+            note:note.value,
+            categorie:categorie.value,
 
 
 
+  }
+tab.push(newdest)
+
+}
+ function opencard(){
+    let 
+ }
 
 
+submit.onclick=function(){
+    
+     }
