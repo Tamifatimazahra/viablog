2d84276e07eb8056ba2f8eff08cbcf8de01f27d2
Tamifatimazahra <tamifatimazahra57@gmail.com>
2026-02-04 16:05:45 +0100

    faire des modification sur js

--- home.js
+++ home.js
@@ -1,24 +1,53 @@
-  let tab=[];
-  function enregister () {
-let title =document.getElementById('title').value;
-let titre =document.getElementById('Titre').value;
-let destination =document.getElementById('destination').value;
-let note =document.getElementById('note').value;
-let categorie =document.getElementById('catego').value;
-let submit =document.getElementById('submit').value;
-let Annuler =document.getElementById('Annuler').value;
-let image =document.getElementById('image').value;
-let newdest={
-            title:title,
-            titre:titre,
-            destination:destination,
-            note:note,
-            categorie:categorie,
-            Image:image,
+let tab = [];
 
-            
-            
-          }
-          tab.push(newdest)
-          
-        }
+
+function enregister() {
+    let titre = document.getElementById('Titre').value;
+    let destination = document.getElementById('destination').value;
+    let note = document.getElementById('note').value;
+    let categorie = document.getElementById('catego').value;
+    let submit = document.getElementById('submit');
+    let Annuler = document.getElementById('Annuler');
+    let image = document.getElementById('img').value;
+}
+let newdest = {
+  titre,
+  destination,
+  note,
+  categorie,
+  image
+};
+tab.push(newdest)
+   let carhtml=` <div class=" flex flex-col justify-center items-center  ">
+        <div class="w-80 h-40  flex  justify-center items-center rounded-t-lg lg:w-[100%]"> <a
+                href="http://127.0.0.1:5501/detail.html" class="lg:w-[80%]"><img
+                    class="w-80 h-40 bg-green-500 flex  justify-center items-center rounded-t-lg lg:w-[100%] "
+                    src="river.jpg" alt=""></a></div>
+    </div>
+
+    <div class="w-80 h-20 bg-stone-100 flex ml-9 rounded-b-lg justify-center items-center lg:flex lg:justify-between lg:items-center lg:m-auto lg:w-[80%]">
+        <div class=" flex   w-[90%] justify-between items-center lg:justify-between lg:gap-12">
+
+            <div class="text-center lg:text-left ">
+                <p  id ="river" class="font-semibold "> </p>
+                <p id ="by" class= "font-light text-xs"> </p>
+            </div>
+            <div class="flex  gap-4 ">
+                <div  class="gap-2 flex "><img src="pointeur-de-localisation.png" alt="" class="w-4 h-4">
+                    <p id ="localisation"  class="text-xs"></p>
+                </div>
+                <div class="gap-2 flex"><img class="w-4 h-4" src="time-and-date.png" alt="">
+                    <p id ="time" class="text-xs"></p>
+                </div>
+                <div  id="oiel" class="gap-2 flex"><img class="w-4 h-4" src="oeil.png" alt="">
+                    <p class="text-xs"></p>
+                </div>
+            </div>
+
+
+
+        </div>
+
+    </div>
+`;
+
