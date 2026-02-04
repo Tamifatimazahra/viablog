0044587e874b94f9550a40584f7a99ab4d1858eb
Tamifatimazahra <tamifatimazahra57@gmail.com>
2026-02-04 17:28:00 +0100

    ajouter cardhtml

--- home.js
+++ home.js
@@ -1,7 +1,18 @@
 let tab = [];
 
+ const form  = document.querySelector(".form")
+ console.log(form)
 
-function enregister() {
+ form.addEventListener('submit' , enregister);
+
+function enregister(e){
+    e.preventDefault();
+}
+
+
+
+function enregister(e) {
+    e.preventDefault()
     let titre = document.getElementById('Titre').value;
     let destination = document.getElementById('destination').value;
     let note = document.getElementById('note').value;
@@ -9,7 +20,7 @@ function enregister() {
     let submit = document.getElementById('submit');
     let Annuler = document.getElementById('Annuler');
     let image = document.getElementById('img').value;
-}
+
 let newdest = {
   titre,
   destination,
@@ -17,30 +28,31 @@ let newdest = {
   categorie,
   image
 };
-tab.push(newdest)
+tab.push(newdest);
+afficherVoyage();
+
    let carhtml=` <div class=" flex flex-col justify-center items-center  ">
-        <div class="w-80 h-40  flex  justify-center items-center rounded-t-lg lg:w-[100%]"> <a
-                href="http://127.0.0.1:5501/detail.html" class="lg:w-[80%]"><img
+        <div class="w-80 h-40  flex  justify-center items-center rounded-t-lg lg:w-[100%]">  lg:w-[80%]><img
                     class="w-80 h-40 bg-green-500 flex  justify-center items-center rounded-t-lg lg:w-[100%] "
-                    src="river.jpg" alt=""></a></div>
+                    src=${image} alt=""></a></div>
     </div>
 
     <div class="w-80 h-20 bg-stone-100 flex ml-9 rounded-b-lg justify-center items-center lg:flex lg:justify-between lg:items-center lg:m-auto lg:w-[80%]">
         <div class=" flex   w-[90%] justify-between items-center lg:justify-between lg:gap-12">
 
             <div class="text-center lg:text-left ">
-                <p  id ="river" class="font-semibold "> </p>
-                <p id ="by" class= "font-light text-xs"> </p>
+                <p  id ="river" class="font-semibold ">${titre} </p>
+                <p id ="by" class= "font-light text-xs"> ${categorie}</p>
             </div>
             <div class="flex  gap-4 ">
                 <div  class="gap-2 flex "><img src="pointeur-de-localisation.png" alt="" class="w-4 h-4">
-                    <p id ="localisation"  class="text-xs"></p>
+                    <p id ="localisation"  class="text-xs"> ${destination}</p>
                 </div>
                 <div class="gap-2 flex"><img class="w-4 h-4" src="time-and-date.png" alt="">
-                    <p id ="time" class="text-xs"></p>
+                    <p id ="time" class="text-xs">${note}</p>
                 </div>
                 <div  id="oiel" class="gap-2 flex"><img class="w-4 h-4" src="oeil.png" alt="">
-                    <p class="text-xs"></p>
+                    <p class="text-xs"> </p>
                 </div>
             </div>
 
@@ -50,4 +62,7 @@ tab.push(newdest)
 
     </div>
 `;
+ container.innerHTML += carhtml;
+ }
+
 
